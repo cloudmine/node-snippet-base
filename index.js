dba1b2992a7ede4c46ef514974c7d2351993ff68
Ethan Mick <ethanmski@gmail.com>
2015-02-05 08:55:52 -0500

    Start server after exporting modules.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var BasicSnippet = require('./lib/basic_snippet');
 var AsyncSnippet = require('./lib/basic_snippet');
-var CloudMineNode = require('cloudmine-node');
-
-CloudMineNode.start('./index.js', function(err) {
-  console.log('Server Started?', err);
-});
+var CloudMineNode = require('cloudmine-node-coderunner');
 
 module.exports = {
   basicSnippet: BasicSnippet,
   AsyncSnippet: AsyncSnippet
 };
+
+CloudMineNode.start(module, './index.js', function(err) {
+  console.log('Server Started?', err);
+});
