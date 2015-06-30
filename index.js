a05bc12374c551000d3cc5cd9bd12574caa79fe0
Ethan Mick <ethan@ethanmick.com>
2015-06-30 13:55:49 -0400

    Nice comments.

--- index.js
+++ index.js
@@ -4,15 +4,28 @@
 // 2015
 //
 
+/**
+ * Require your Snippets
+ * This can be more expressive, for example, you can use `fs` to
+ * read all snippets in a directory and set them to your `module.exports`.
+ */
 var BasicSnippet = require('./lib/basic_snippet');
 var AsyncSnippet = require('./lib/async_snippet');
 var CloudMineNode = require('cloudmine-servercode');
+// Require any other node module you want...
 
+/**
+ * The `module.exports` **must** be called before the server is started,
+ * or the server won't be able to read in the exports.
+ */
 module.exports = {
   basic: BasicSnippet,
   async: AsyncSnippet
 };
 
+/**
+ * Start the CloudMine server.
+ */
 CloudMineNode.start(module, './index.js', function(err) {
   console.log('Server Started?', err);
 });
