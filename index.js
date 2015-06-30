871d1e514c9aa4e9f92c6cbb9fc9fafd37656208
Ethan Mick <ethan@ethanmick.com>
2015-06-30 13:53:56 -0400

    nice headers

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+'use strict';
+//
+// CloudMine, Inc.
+// 2015
+//
+
 var BasicSnippet = require('./lib/basic_snippet');
 var AsyncSnippet = require('./lib/async_snippet');
 var CloudMineNode = require('cloudmine-servercode');
